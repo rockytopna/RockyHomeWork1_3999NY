166a91a63a268e98f50b7787b9241333d0870810
rockytopna <rockytopna@gmail.com>
2020-01-25 01:46:22 -0500

    AA

---
+++ AA/src/main/java/test/DropDown.java
@@ -0,0 +1,24 @@
+package test;
+
+import common.CommonAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class DropDown extends CommonAPI {
+
+    @Test
+    public void bookTicketTest1()throws InterruptedException {
+        typeOnElement("input#reservationFlightSearchForm\\.originAirport","JFK");
+        typeOnElement("/html//input[@id='reservationFlightSearchForm.destinationAirport']","GRR");
+        Thread.sleep(1000);
+    }
+
+    @Test (enabled = false)
+    public void dropDownTest2() throws InterruptedException {
+        bookTicketTest1();
+        Select dropDown = new Select (driver.findElement(By.name("adultOrSeniorPassengerCount")));
+        dropDown.selectByValue("2");
+    Thread.sleep(1000);
+    }
+}
--- Supsew/src/main/java/home/HomePage.java
+++ Supsew/src/main/java/home/HomePage.java
@@ -6,13 +6,13 @@ import org.testng.annotations.Test;
 
 public class HomePage extends CommonAPI {
 
-    @Test
+    @Test (enabled = false)
     public void supSewLogoTest1() throws InterruptedException {
         clickOnElement("ul#menu-top-menu  a[target='_blank']");
         Thread.sleep(1000);
     }
 
-    @Test
+    @Test (enabled = false)
     public void signInTest2() throws InterruptedException {
         driver.get("https://ecomm.supsew.com/Login.aspx");
         driver.findElement(By.xpath("//*[@id=\"txtUserName\"]")).sendKeys("rockytopna@gmail.com");
@@ -22,13 +22,26 @@ public class HomePage extends CommonAPI {
 
     }
 
-    @Test
+    @Test (enabled =false)
     public void featuredProdectLookupTest3() throws InterruptedException {
         driver.findElement(By.xpath("//li[@id='menu-item-25443']//a")).click();
         Thread.sleep(1000);
 
     }
 
+    @Test
+    public void sendMessagetoNYOfficeTest4() throws  InterruptedException{
+
+        driver.findElement(By.xpath("//li[@id='menu-item-134797']//a[contains(text(),'New York')]")).click();
+        typeOnElement("template-contactform-name", "Tonny");
+        typeOnElement("template-contactform-email", "Tonny@gmail.com");
+        typeOnElement("template-contactform-phone","2124567546,");
+        typeOnElement("template-contactform-message","Hello");
+        clickOnElement("//button[@id='template-contactform-submit']");
+        Thread.sleep(1000);
+
+
+    }
 
 
 
